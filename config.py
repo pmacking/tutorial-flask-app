b7e0e7b1e2362346cbd670407c9394a7f52ae866
pmacking <pmacking@users.noreply.github.com>
2020-05-28 22:48:27 -0400

    Bugfix typo BASEDIR

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ app = connexion_app.app
 # config SQLAlchemy component of Flask app instance, /// supports Windows OS
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'data/yahtzee.db')
+    'sqlite:///' + os.path.join(BASEDIR, 'data/yahtzee.db')
 app.config['SQLALCHMEY_TRACK_MODIFICATIONS'] = False
 
 # create SQLAlchemy db instance
