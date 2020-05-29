1b84722108fd4f938ad4714340c75d7258d52adc
pmacking <pmacking@users.noreply.github.com>
2020-05-29 14:15:26 -0400

    Adds basic User model

---
+++ yahtzee/models.py
@@ -0,0 +1,33 @@
+"""
+This module contains the models for yahtzee. It also makes use of SQLAlchemy
+"Model" class and Marshmallow "SQLAlchemyAutoSchema" class inheretence.
+"""
+
+from datetime import datetime
+from config import db, ma
+
+
+class User(db.Model):
+    """
+    User model which defines the user attributes and SQLite3 db table/fields.
+    """
+    __table__ = "user"
+    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
+    username = db.Column(db.String(32), nullable=False)
+    first_name = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
+    timestamp = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utc.now
+        )
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    """
+    This user schema inherets from SQLAlchemyAutoSchema and uses the Meta
+    class to find the SQLAlchemy model User and the db.session.
+    """
+    class Meta:
+        model = User
+        sqla_session = db.session
