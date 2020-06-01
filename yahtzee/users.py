f86606d4bfd22dec1db6938de5121dceead43d84
pmacking <pmacking@users.noreply.github.com>
2020-06-01 07:39:56 -0400

    Updates read_all handler to return list of users by last_name

--- yahtzee/users.py
+++ yahtzee/users.py
@@ -3,12 +3,37 @@ This module contains users.CRUD operations as a handler for HTTP requests to
 /api/users
 """
 
+# import flask modules to create REST API responses
+from flask import (
+    make_response,
+    abort,
+)
+
+from config import db
+
+# import SQLAlchemy User and Marshmallow UserSchema classes to access user
+# database table and serialize the results
+from models import (
+    User,
+    UserSchema,
+)
+
 
 # create handler for read (GET) users
-def read():
+def read_all():
     """
-    This function respons to a request for api/users with the list of users
+    This function responds to a request for api/v1/users with the list of users
 
     :return:            sorted list of users
     """
-    pass
+    # create the list of users from our data
+    users = User.query \
+        .order_by(User.last_name) \
+        .all()
+
+    # serialize data for response: many=True tells UserSchema expect iterable
+    user_schema = UserSchema(many=True)
+    # The return result is an object having a data attribute, an object
+    # containing a people list that can be converted to JSON. This is returned
+    # and converted by Connexion to JSON as the response to the REST API call
+    return user_schema.dump(users).data
