6fd45de5a79eb66e0abfac67c67f777f3eb00f1f
pmacking <pmacking@users.noreply.github.com>
2020-07-10 11:15:51 -0400

    Adds error Blueprint, handlers, templates

--- yahtzee/__main__.py
+++ yahtzee/__main__.py
@@ -5,10 +5,11 @@ Main module of the server file.
 from yahtzee import app
 from yahtzee.users.routes import users
 from yahtzee.main.routes import main
-from yahtzee.users.forms import RegistrationForm, LoginForm
+from yahtzee.errors.handlers import errors
 
 app.register_blueprint(users)
 app.register_blueprint(main)
+app.register_blueprint(errors)
 
 if __name__ == "__main__":
     app.run()
